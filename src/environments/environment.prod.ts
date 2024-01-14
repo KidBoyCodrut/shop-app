5637fbaa9b41ae3c2b2d0ba3739a3a021f4973a7
Codrut <cstyle.kidboy@yahoo.com>
2024-01-14 11:00:48 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDMWBn9Zt6c1GBs-oYksnUI1BXS_FvnfAw",
+    authDomain: "shop-app-ea591.firebaseapp.com",
+    projectId: "shop-app-ea591",
+    storageBucket: "shop-app-ea591.appspot.com",
+    messagingSenderId: "651613997541",
+    appId: "1:651613997541:web:5abdd380e60f851eda470c",
+    measurementId: "G-CWNT5TXBP4"
+  }
 };
